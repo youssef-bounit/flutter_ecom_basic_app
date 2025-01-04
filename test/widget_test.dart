1277e2c3ece325dc3717ed11fb7cfd31975bc971
Youssef Bounit <youyoussef769@hotmail.com>
2025-01-04 11:34:39 +0100

    add login page

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_ecom_basic_app/main.dart';
-import 'package:flutter_ecom_basic_app/pages/app_state.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 
